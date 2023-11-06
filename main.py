b0d4e46b43f01a462caa73c80daf366fb81e6168
Aditya Kumar <aaz22hsns@gmail.com>
2023-11-06 15:57:37 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+import os
+
+abs_path = os.getcwd()
+
+def main():
+    img = cv.imread(os.path.join(abs_path,'sketch check.jpg'))
+
+    
+if __name__ == '__main__':
+    main()
+    
