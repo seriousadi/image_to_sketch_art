aa31c610db80f3c74a2f059b1cdda2760d78fd7d
Aditya Kumar <aaz22hsns@gmail.com>
2023-11-08 13:20:58 +0530

    make a way convert the image to grayscale

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import numpy as np
 import cv2 as cv
 import os
 
-abs_path = os.getcwd()
 
 def main():
-    img = cv.imread(os.path.join(abs_path,'sketch check.jpg'))
 
-    
+    abs_path = os.getcwd()  # get absolute path of current working directory
+
+    img = cv.imread(os.path.join(
+        abs_path, 'sketch check.jpg'))  # get image and convert it to numpy array
+
+    # converted pixels of coloured image will be stored here (will be used later)
+    graysclae_pixels = []
+
+    # grayscale the image
+    for _ in img:
+        converted_pixels = []
+        for n in _:
+            # converting coloured values to grayscale using NTSC formula
+            g = 0.299*n[0] + 0.587*n[1] + 0.114 * n[2]
+            converted_pixels.append(g)
+        graysclae_pixels.append(converted_pixels)
+
+    grayscale_img = np.array(graysclae_pixels)
+
+
 if __name__ == '__main__':
     main()
-    
